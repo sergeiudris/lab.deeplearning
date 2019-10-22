a18a0bbf1577c792eca6edbec768af95d6c62c08
seeris <sergei.udris@gmail.com>
2019-10-22 13:55:29 +0300

    ndarrays

--- d2l/src/d2l/core.clj
+++ d2l/src/d2l/core.clj
@@ -36,11 +36,52 @@
   (nd/ones [2 3 4])
 
   (nd/array (flatten [[2 1 4] 3 [1 2 3 4] [4 3 2 1]]) [3 4])
-  
-  (random/normal 0 1 [3 4])
-  
-  
 
+  (random/normal 0 1 [3 4])
+
+  (def x2 (nd/array [1 2 4 8] [4]))
+  (def x3 (nd/* (nd/ones-like x2) 2))
+  (nd/+ x2 x3)
+  (nd/- x2 x3)
+  (nd/* x2 x3)
+  (nd// x2 x3)
+  (nd/exp x2)
+  
+   ; inplace
+  (nd/+= x2 x3)
+
+  (def x (-> (nd/arange 0 12) (nd/reshape [3 4])))
+  (def y (nd/array (flatten [[2 1 4 3] [1 2 3 4] [4 3 2 1]]) [3 4]))
+  (nd/dot x (nd/transpose y))
+
+  (nd/concatenate [x y] {:axis 0})
+  (nd/concatenate [x y] {:axis 1})
+  
+  (nd/equal x y)
+  (nd/< x y)
+  (nd/>= x y)
+  
+  
+  (nd/sum x)
+  (nd/->double-vec (nd/norm (nd/sum x)))
+  (nd/->raw (nd/sum x))
+  (nd/->vec (nd/sum x))
+  
+  (nd/->vec (nd/norm x))
+  (first (nd/->vec (nd/norm x)))
+  
+  (def a (-> (nd/arange 0 3) (nd/reshape [3 1])))
+  (def b (-> (nd/arange 0 2) (nd/reshape [1 2])))
+  
+  (def c (nd/broadcast-add a b))
+  
+  (nd/slice c 0 )
+  
+  
+ 
+  
+  
+  
   ;
   )
 
