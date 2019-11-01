e7f9ab16c1a3a3a19b8fd15c8bba14fde1a45af0
seeris <sergei.udris@gmail.com>
2019-11-01 14:04:39 +0300

    reading arxiv xml

---
+++ d2l/src/d2l/arxiv.clj
@@ -0,0 +1,56 @@
+(ns d2l.arxiv
+  (:require [clojure.reflect :refer [reflect]]
+            [clojure.pprint :as pp]
+            [clojure.java.io :as io]
+            [clojure.java.shell :refer [sh]]
+            [clojure.string :as str]
+            [clojure.data.csv :refer [read-csv]]
+            [clojure.data.xml :as xml]
+            [clojure.zip :as zip]
+            [clojure.xml]
+            [pad.coll.core :refer [contained?]]
+            [pad.io.core :refer [read-nth-line count-lines]]
+            [pad.core :refer [str-float? str>>float resolve-var]]
+            [pad.math.core :refer [vec-standard-deviation-2
+                                   scalar-subtract elwise-divide
+                                   vec-mean scalar-divide
+                                   mk-one-hot-vec std]]
+            [org.apache.clojure-mxnet.io :as mx-io]
+            [org.apache.clojure-mxnet.context :as context]
+            [org.apache.clojure-mxnet.module :as m]
+            [org.apache.clojure-mxnet.symbol :as sym]
+            [org.apache.clojure-mxnet.kvstore :as kvstore]
+            [org.apache.clojure-mxnet.kvstore-server :as kvstore-server]
+            [org.apache.clojure-mxnet.eval-metric :as eval-metric]
+            [org.apache.clojure-mxnet.optimizer :as optimizer]
+            [org.apache.clojure-mxnet.lr-scheduler :as lr-scheduler]
+            [org.apache.clojure-mxnet.initializer :as initializer]
+            [org.apache.clojure-mxnet.resource-scope :as resource-scope]
+            [org.apache.clojure-mxnet.ndarray :as nd]
+            [org.apache.clojure-mxnet.dtype :as dtype]
+            [org.apache.clojure-mxnet.callback :as callback]
+            [org.apache.clojure-mxnet.layout :as layout]
+            [org.apache.clojure-mxnet.random :as random]
+            [org.apache.clojure-mxnet.shape :as shape])
+  (:gen-class))
+
+(def data-dir "./tmp/data/arxiv/")
+
+#_(with-open [input (java.io.FileInputStream. (str data-dir "oai2-cs-1000.xml"))]
+    (let [data (xml/parse input)]
+      (prn data)))
+
+#_(def cs-data (with-open [input (java.io.FileInputStream. (str data-dir "oai2-cs-1000.xml"))]
+                 (->> input
+                      (xml/parse)
+                      (:content)
+                      (take 25)
+                      )))
+#_(def cs-data (clojure.xml/parse (str data-dir "oai2-cs-1000.xml")))
+#_(keys cs-data)
+#_(:attrs cs-data)
+#_(->> cs-data :content (last) :content (count)) ; 1001, last element is resumtionToken
+
+#_(def records (->> cs-data :content (last) :content (butlast)))
+#_(count records)
+#_(first records)
--- d2l/src/d2l/main.clj
+++ d2l/src/d2l/main.clj
@@ -6,9 +6,10 @@
             [d2l.mxnet-made-simple]
             [d2l.viz]
             [d2l.inception]
+            [d2l.arxiv]
             [bert.bert-sentence-classification]
             [cnn-text-classification.classifier]
-   
+
    ;
             ))
 
