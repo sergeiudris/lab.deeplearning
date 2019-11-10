e05e3213a02a754f61dc5576dde096d49003cfab
seeris <sergei.udris@gmail.com>
2019-11-10 09:16:21 +0300

    tf

--- tf/src/tf/tensorflow.clj
+++ tf/src/tf/tensorflow.clj
@@ -3,53 +3,16 @@
   (:import
    (org.tensorflow Graph Session Tensor TensorFlow)))
 
-(defn version
-  []
-  (TensorFlow/version))
 
-(defn graph
-  []
-  (Graph.))
-
-(defn tensor
-  ([a]
-   (Tensor/create a))
-  ([a b]
-   (Tensor/create a b))
-  ([a b c]
-   (Tensor/create a b c)))
-
-#_(def g (graph))
-#_(def t (tensor (.getBytes "hello" "UTF-8") ))
-
-(defn op-builder
-  [g a b]
-  (.opBuilder g a b))
-
-(defn set-attr
-  [g a b]
-  (.setAttr g a b))
-
-(defn t>datatype
-  [t]
-  (.dataType t))
-
-(defn build
-  [g]
-  (.build g))
-
-(defn session
-  [g]
-  (Session. g))
-
-#_(let [t (tensor (.getBytes "hello!" "UTF-8"))
-        g (graph)]
+#_(let [v (.getBytes "hello :)" "UTF-8")
+        t (Tensor/create v)
+        g (Graph.)]
     (do (-> g
-            (op-builder "Const" "MyConst")
-            (set-attr "dtype" (t>datatype t))
-            (set-attr "value" t)
-            (build)))
-    (let [sess (session g)
+            (.opBuilder "Const" "MyConst")
+            (.setAttr "dtype" (.dataType t))
+            (.setAttr "value" t)
+            (.build)))
+    (let [sess (Session. g)
           tensor-out (-> sess
                          (.runner)
                          (.fetch "MyConst")
