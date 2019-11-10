6beaec89117b8e3923a1a5fda001a77ed27ab46a
seeris <sergei.udris@gmail.com>
2019-11-10 15:00:05 +0300

    baseball data

--- tfjs/src/app/baseball.cljs
+++ tfjs/src/app/baseball.cljs
@@ -1,12 +1,50 @@
 (ns app.baseball
-  (:require ["@tensorflow/tfjs" :as tf]
+  (:require [clojure.string :as string]
+            ["@tensorflow/tfjs" :as tf]
             ["fs" :as fs]
-            ["child_process" :as cp]))
+            ["path" :as path]
+            ["child_process" :as cp]
+            ["readline" :as readline]))
 
+#_(path/resolve ".")
 
-(def data-dir "./tmp/data/baseball")
+(def data-dir (str (path/resolve "./tmp/data/baseball/") "/") )
 
 (defn load-data!
   []
   #_(-> (cp/execSync "ls") (str))
-  (cp/execSync "bash data.sh baseball"))
+  (-> (cp/execSync "bash data.sh baseball") (str)))
+
+#_(load-data!)
+
+(defn slurp-csv
+  [filename]
+  (as-> filename x
+    (fs/readFileSync x)
+    (.toString x)
+    (string/split x  #"\n")
+    (mapv #(string/split % #",") x)))
+
+#_(do
+    (def file (-> (str data-dir "pitch_type_training_data.csv") (fs/readFileSync) (.toString)))
+    (def lines (string/split file #"\n"))
+    (def header (first lines))
+    (def rows (rest lines))
+    (def data (->> rows (mapv #(string/split % #","))))
+    )
+
+#_(count lines)
+#_(count data)
+#_(-> rows (last) (string/split #","))
+#_(-> header (string/split #","))
+#_(->> data (first))
+
+#_(do
+    (def csv-training (slurp-csv (str data-dir "pitch_type_training_data.csv")))
+    (def csv-test (slurp-csv (str data-dir "pitch_type_test_data.csv"))))
+
+#_(second csv-training)
+#_(first csv-test)
+
+
+
