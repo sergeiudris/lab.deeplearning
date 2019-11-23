01233f2a5aff9d19fb6e454afee69186667b6388
seeris <sergei.udris@gmail.com>
2019-11-23 07:41:19 +0300

    :wip/gvm eval and exec a js fn

--- gvm/src/gvm/polyglot.clj
+++ gvm/src/gvm/polyglot.clj
@@ -13,6 +13,13 @@
   (type langs)
 
   (.eval ctx "js" "print('Hello world!')")
+  
+  (def f (.eval ctx "js" "(x)=> x + 1"))
+  (.canExecute f)
+  (type (. clojure.lang.RT object_array [1 "2"]))
+  (def vl (.execute f (object-array [41])))
+  (.asInt vl)
+  (type (.asInt vl))
   (.close ctx)
 
   ;
